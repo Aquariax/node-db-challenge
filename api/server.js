121c7da94eb0e04bbb828c19179226ea33121ab1
Aquariax <roberthrrs8@gmail.com>
2019-07-19 11:57:32 -0400

    server setup, index and server files set up

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const server = express()
+const helmet = require('helmet');
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res)=>{
+    res.send("api up and running");
+})
+module.exports = server;
